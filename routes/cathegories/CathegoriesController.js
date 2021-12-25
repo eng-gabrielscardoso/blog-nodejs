35f3493c28d53ea8c9e40f65871ceda026ea08ba
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-12-25 14:07:01 -0300

    refactor: refactor index in two components

--- routes/cathegories/CathegoriesController.js
+++ routes/cathegories/CathegoriesController.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/admin/categorias/novo', (req, res, next) => {
-  
+  res.render('admin/cathegories/new');
 });
 
 module.exports = router;
