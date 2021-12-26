84d4da6341f796cdd9e19453d45c9e32c82dac56
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-12-26 12:38:33 -0300

    feat: add delete cathegory action

--- routes/cathegories/CathegoriesController.js
+++ routes/cathegories/CathegoriesController.js
@@ -3,10 +3,10 @@ const express = require('express');
 const slugify = require('slugify');
 const router = express.Router();
 
-const Cathegory  = require('./Cathegory');
+const CathegoryModel = require('./Cathegory');
 
 router.get('/admin/categorias', (req, res, next) => {
-  Cathegory.findAll().then(cathegories => {
+  CathegoryModel.findAll().then(cathegories => {
     res.render('admin/cathegories/index', {
       cathegories: cathegories
     });
@@ -20,18 +20,42 @@ router.get('/admin/categorias/novo', (req, res, next) => {
 router.post('/categorias/salvar', (req, res, next) => {
   let newCathegory = req.body.cathegoryNameRegister;
 
-  if(!newCathegory || newCathegory == undefined) {
+  if (!newCathegory || newCathegory == undefined) {
     res.redirect('/admin/categorias/novo');
   } else {
-    Cathegory.create({
+    CathegoryModel.create({
       title: newCathegory,
       slug: slugify(newCathegory)
-    }).then(_ => res.redirect('/'))
+    }).then(_ => res.redirect('/admin/categorias'))
       .catch(e => {
-        console.log(`An error occurred during register a new cathegory. Log: ${e}`)
-        res.redirect('/admin/categorias/novo')
+        console.log(`An error occurred during register a new cathegory. Log: ${e}`);
+        res.redirect('/admin/categorias/novo');
       });
-  }
-})
+  };
+});
+
+router.post('/admin/categorias/deletar', (req, res, next) => {
+  let cathegoryId = req.body.cathegoryId;
+
+  if (cathegoryId != undefined) {
+    if (!isNaN(cathegoryId)) {
+      CathegoryModel.destroy({
+        where: {
+          id: cathegoryId
+        }
+      }).then(_ => {
+        res.redirect('/admin/categorias');
+      })
+        .catch(e => {
+          console.log(`An error occurred during delete a cathegory. Log: ${e}`);
+          res.redirect('/admin/categorias');
+        });
+    } else {
+      res.redirect('/admin/categorias');
+    };
+  } else {
+    res.redirect('/admin/categorias');
+  };
+});
 
 module.exports = router;
