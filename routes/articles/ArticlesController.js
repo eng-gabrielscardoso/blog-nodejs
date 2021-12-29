03fef320e566285fa2cfb0297f747477594b580a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-12-28 23:00:52 -0300

    feat: tinymce editor integration

--- routes/articles/ArticlesController.js
+++ routes/articles/ArticlesController.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 
+const CategoryModel = require('../categories/Category');
+
 router.get('/admin/artigos/novo', (req, res, next) => {
-  res.render('admin/articles/new')
+  CategoryModel.findAll()
+    .then(categories => {
+      res.render('admin/articles/new', { categories: categories, });
+    });
 });
 
 module.exports = router;
