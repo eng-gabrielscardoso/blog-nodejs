c7bb747ae315a0895908b7fda4ca9e4e3eaa658c
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-01-02 14:32:23 -0300

    feat: save articles route
    BREAKING CHANGE: add a new environmental variable: DB_TIMEZONE

--- private/database/connection.js
+++ private/database/connection.js
@@ -7,7 +7,8 @@ const connection = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    timezone: process.env.DB_TIMEZONE,
 });
 
 module.exports = connection;
--- routes/articles/ArticlesController.js
+++ routes/articles/ArticlesController.js
@@ -1,14 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const slugify = require('slugify');
 const router = express.Router();
 
+const ArticleModel = require('../articles/Article');
 const CategoryModel = require('../categories/Category');
+
+router.get('/admin/artigos', (req, res, next) => {
+  res.send('Rota de artigos');
+});
 
 router.get('/admin/artigos/novo', (req, res, next) => {
   CategoryModel.findAll()
     .then(categories => {
       res.render('admin/articles/new', { categories: categories, });
     });
+});
+
+router.post('/admin/artigos/salvar', (req, res, next) => {
+  let articleTitle = req.body.articleTitleRegister;
+  let articleBody = req.body.articleBodyRegister;
+  let articleCategory = req.body.articleCategoryRegister.value;
+
+  ArticleModel.create({
+    title: articleTitle,
+    slug: slugify(articleTitle),
+    body: articleBody,
+    categoryId: articleCategory,
+  }).then(_ => res.redirect('/admin/artigos'))
 });
 
 module.exports = router;
